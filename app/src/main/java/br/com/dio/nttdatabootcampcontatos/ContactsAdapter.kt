0e48d51abae576846fa82807bb7301352ba71bef
Paulo Normando <paulonormando@gmail.com>
2022-07-30 19:29:43 -0300

    Update on MainActivy, created an cursor for adapter

--- app/src/main/java/br/com/dio/nttdatabootcampcontatos/ContactsAdapter.kt
+++ app/src/main/java/br/com/dio/nttdatabootcampcontatos/ContactsAdapter.kt
@@ -1,12 +1,16 @@
 package br.com.dio.nttdatabootcampcontatos
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class ContactsAdapter(val contactsList: ArrayList<Contact>): RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_view, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactsAdapter.ViewHolder, position: Int){
@@ -15,6 +19,19 @@ class ContactsAdapter(val contactsList: ArrayList<Contact>): RecyclerView.Adapte
 
     override fun getItemCount(): Int {
         return contactsList.size
+    }
+
+    class ViewHolder(itemView: View): RecycleView.ViewHolder(itemView) {
+
+        fun bindItem(contact: Contact){
+
+            val textName = itemView.findViewById<Text>(R.id.contact_name)
+            val textPhone = itemView.findViewById<TextView>(R.id.contact_phone_number)
+
+            textName.text = contact.name
+            textPhone.text = contact.phoneNumber
+        }
+
     }
 
 }
